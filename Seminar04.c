29c2d8b2ec47dacabccf8c48367897a15faacec3
StorociucAna <storociucana17@stud.ase.ro>
2024-03-19 20:39:23 +0200

    Seminar5

--- Seminar04.c
+++ Seminar04.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -161,4 +161,4 @@ void main() {
 	dezalocareMatrice(&aeroporturi, &nrColoane, &nrLinii);
 
 
-}
+}*/
